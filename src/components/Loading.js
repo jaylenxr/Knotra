ac2229bca07839adccc73402ca4ad735651db2ef
Jaylen Webb <jayrw6@gmail.com>
2025-03-15 14:00:28 -0500

    fixed minor favorites issue

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,11 +4,7 @@ import { PulseLoader } from 'react-spinners';
 export default function Loading() {
   return (
     <div className="text-center mt-5">
-      <PulseLoader
-        color="#FFB6A1" // Peach color from your palette (use any color from your scheme)
-        size={20} // Adjust the size as needed
-        margin={5} // Adjust space between pulses
-      />
+      <PulseLoader color="#FFB6A1" size={20} margin={5} />
     </div>
   );
 }
