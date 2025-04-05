8db28fd03e5be098829b47e63d9964ba9774eefa
Jaylen Webb <jayrw6@gmail.com>
2025-04-05 08:26:40 -0500

    added a little styling

--- src/app/profile/[firebaseKey]/page.js
+++ src/app/profile/[firebaseKey]/page.js
@@ -1,7 +1,9 @@
 'use client';
 
 import { useAuth } from '@/utils/context/authContext';
-import React, { useState, useEffect, useCallback } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
 import LogCard from '../../../components/LogCard';
 import { getLogsByUid } from '../../../api/logData';
 
@@ -9,6 +11,7 @@ function Profile() {
   const { user } = useAuth();
   const [currentLogs, setCurrentLogs] = useState([]);
 
+  // Fetch logs
   const getCurrentLogs = useCallback(() => {
     getLogsByUid(user.uid).then(setCurrentLogs);
   }, [user.uid]);
@@ -21,6 +24,13 @@ function Profile() {
     <div className="profile-container">
       <h3 className="profile-title">My Logs</h3>
       <div className="profile-grid">{currentLogs.length === 0 ? <h5 className="no-logs">No current logs</h5> : currentLogs.map((log) => <LogCard key={log.firebaseKey} logObj={log} onUpdate={getCurrentLogs} />)}</div>
+      <div className="new-log">
+        <Link href="/log/new" passHref>
+          <Button variant="success" className="log-button">
+            new log
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,56 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { signIn } from '../utils/auth';
 
 function Signin() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '100vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-        backgroundColor: '#f1f0e6',
-        borderRadius: '10px',
-        boxShadow: '0 10px 30px rgba(0, 0, 0, 0.1)',
-      }}
-    >
-      <h1
-        style={{
-          color: '#ff6f61',
-          fontFamily: '"Helvetica Neue", sans-serif',
-          fontWeight: 'bold',
-        }}
-      >
-        Knotra
-      </h1>
-      <p
-        style={{
-          color: '#6a7f49',
-          marginBottom: '20px',
-        }}
-      >
-        Your Hair Health Journey Starts Here
-      </p>
-      <Button
-        type="button"
-        size="lg"
-        className="copy-btn"
-        onClick={signIn}
-        style={{
-          backgroundColor: '#ff6f61',
-          border: 'none',
-          padding: '15px 30px',
-          borderRadius: '5px',
-          color: 'white',
-          fontWeight: 'bold',
-          fontSize: '18px',
-          transition: 'background-color 0.3s ease',
-        }}
-      >
-        Sign In
-      </Button>
+    <div className="signin-wrapper">
+      <img alt="knotra-background" src="knotrabackground.png" className="signin-background" />
+
+      <div className="signin-content">
+        <Button type="button" size="lg" onClick={signIn} className="signin-button">
+          Sign In
+        </Button>
+      </div>
     </div>
   );
 }
